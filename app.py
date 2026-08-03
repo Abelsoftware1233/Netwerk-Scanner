f070d7834ee0d99295525f6f160deef83ecaa20e
A.A <abelsoftware123@hotmail.com>
2026-08-03 20:09:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,6 @@ def api_attack_surface():
 
 if __name__ == "__main__":
     print("=" * 50)
-    print("  NetScanner Pro – starting on http://localhost:5000")
+    print("  NetScanner Pro – starting on http://localhost:5008")
     print("=" * 50)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5008)
