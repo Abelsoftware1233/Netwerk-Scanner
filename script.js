3c3c4fcbda0e15e6853f3c99a288a065cf22c321
A.A <abelsoftware123@hotmail.com>
2026-08-16 02:00:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ─── State ─────────────────────────────────────────────────────────────── */
-const API = window.location.protocol + "//" + window.location.host + "/api";
+const API = "http:87.106.41.140:5008/api";
 let selectedHost = null;
 let hosts = [];
 let radarAngle = 0;
